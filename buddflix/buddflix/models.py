ee72fb6f996e26d454c08b482337e14552878aa8
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-15 14:51:03 -0400

    updated requirements to include psycopg2, changed to a postgres setup, removed explicitly defined join tables

--- buddflix/buddflix/models.py
+++ buddflix/buddflix/models.py
@@ -5,11 +5,6 @@ import datetime
 class Type(models.Model):
     name = models.CharField(max_length=255)
 
-class Strain(models.Model):
-    name = models.CharField(max_length=255)
-    type = models.ForeignKey(
-        Type, on_delete=models.CASCADE, related_name='strains')
-
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
@@ -18,13 +13,12 @@ class Effect(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name='effects')
 
-class StrainToEffect(models.Model):
-    strain = models.ForeignKey(Strain, on_delete=models.CASCADE, related_name='strain_to_effect')
-    effect = models.ForeignKey(Effect, on_delete=models.CASCADE, related_name='strain_to_effect')
-
 class Flavour(models.Model):
     name = models.CharField(max_length=255)
 
-class StrainToFlavour(models.Model):
-    strain = models.ForeignKey(Strain, on_delete=models.CASCADE, related_name='strain_to_flavours')
-    flavour = models.ForeignKey(Flavour, on_delete=models.CASCADE, related_name='strain_to_flavours')
+class Strain(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.ForeignKey(
+        Type, on_delete=models.CASCADE, related_name='strains')
+    effects = models.ManyToManyField(Effect, related_name='strains')
+    flavours = models.ManyToManyField(Flavour, related_name='strains')
