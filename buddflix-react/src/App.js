8a80063c649807f95bd4ed6383692c2ca8278f9c
Ben Lardie <benlardie@gmail.com>
2019-04-15 13:54:41 -0400

    created categorey component

--- buddflix-react/src/App.js
+++ buddflix-react/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AgeVerification from './AgeVerification';
+import Categories from './Categories';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           BuddFlix 
           <AgeVerification />
+          <Categories />
           
         </header>
       </div>
---
+++ buddflix-react/src/Categories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Categories = () => {
+ return (
+     <h1>Hello World Categories</h1>
+ )
+}
+
+export default Categories;
