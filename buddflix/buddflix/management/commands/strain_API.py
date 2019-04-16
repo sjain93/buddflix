6907fa7aa4de9639a15d5f89f860ae9bf4d22504
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-04-16 16:39:53 -0400

    minor fix

--- buddflix/buddflix/management/commands/strain_API.py
+++ buddflix/buddflix/management/commands/strain_API.py
@@ -1,11 +1,13 @@
 import json
 import requests
+import os 
+key = os.environ.get("STRAIN_KEY")
 
 # this file needs to reside inside a management folder in the django app
 
 def effect_data():
     # Importing effects 
-    res = requests.get("http://strainapi.evanbusse.com/znqfSrD/searchdata/effects")
+    res = requests.get("http://strainapi.evanbusse.com/{}/searchdata/effects".format(key))
     body = json.loads(res.content)
     positive_effect = []
     negative_effect = []
@@ -24,7 +26,7 @@ def effect_data():
 
 def category_data():
     # Importing type (positive, negative, medical)
-    res = requests.get("http://strainapi.evanbusse.com/znqfSrD/searchdata/effects")
+    res = requests.get("http://strainapi.evanbusse.com/{}/searchdata/effects".format(key))
     body = json.loads(res.content)
     category_seed = []
 
@@ -37,7 +39,7 @@ print(category_data())
 
 def flavours_data():
       # Importing flavours data already in Array
-    res = requests.get("http://strainapi.evanbusse.com/znqfSrD/searchdata/flavors")
+    res = requests.get("http://strainapi.evanbusse.com/{}/searchdata/flavors".format(key))
     body = json.loads(res.content)
     flavours_seed = body
     return flavours_seed
