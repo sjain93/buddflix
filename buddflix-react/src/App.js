58ea628b07b94f924e54978942b327918f587f01
Ben Lardie <benlardie@gmail.com>
2019-04-16 15:09:39 -0400

    added hover effect

--- buddflix-react/src/App.js
+++ buddflix-react/src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
-import Navigation from './Navigation';
+import Navigation from './Navigation/Navigation';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          BuddFlix 
           <Navigation />
           
-        </header>
-      </div>
+        </div>
     );
   }
 }
--- buddflix-react/src/Navigation/Navigation.js
+++ buddflix-react/src/Navigation/Navigation.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Categories from "./Categories";
-import About from './About';
-import Home from './Home';
+import './Nav.scss'
+import logo from '../logo.png';
+import Categories from "../Categories";
+import About from '../About';
+import Home from '../Home';
 
 const Navigation = () => {
     return (
     <Router>
-            <div>
+            <header>
+            <img src={logo} className="App-logo" alt="logo" />
             <nav>
                 <ul>
-                    <li>
+                    <li className='link-hover'>
                         <Link to="/">Home</Link>
                     </li>
-                    <li>
+                    <li className='link-hover'>
                         <Link to="/about">About</Link>
                     </li>
-                    <li>
+                    <li className='link-hover'>
                         <Link to="/categories">Categories</Link>
                     </li>
                 </ul>
@@ -25,7 +28,7 @@ const Navigation = () => {
             <Route path="/" exact component={Home} />
             <Route path="/about" exact component={About} />
             <Route path="/categories" exact component={Categories} />
-            </div>
+            </header>
     </Router>
 );
 }
