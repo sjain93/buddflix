15d0340e3cf109118720dba57383e9b069177493
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-16 10:44:44 -0400

    changed the name of the Type model to Race

--- buddflix/buddflix/models.py
+++ buddflix/buddflix/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 
-class Type(models.Model):
+class Race(models.Model):
     name = models.CharField(max_length=255)
 
 class Category(models.Model):
@@ -10,15 +10,13 @@ class Category(models.Model):
 
 class Effect(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name='effects')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='effects')
 
 class Flavour(models.Model):
     name = models.CharField(max_length=255)
 
 class Strain(models.Model):
     name = models.CharField(max_length=255)
-    type = models.ForeignKey(
-        Type, on_delete=models.CASCADE, related_name='strains')
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='strains')
     effects = models.ManyToManyField(Effect, related_name='strains')
     flavours = models.ManyToManyField(Flavour, related_name='strains')
