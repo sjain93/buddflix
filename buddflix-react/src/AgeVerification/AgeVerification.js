e945057359f624a246619effa7428b85a61220ff
Vikil Naik <vikilnaik37@gmail.com>
2019-04-16 11:29:10 -0400

    Created a validation form for inputting age, users must be 19 years or older in order to enter our website

--- buddflix-react/src/AgeVerification/AgeVerification.js
+++ buddflix-react/src/AgeVerification/AgeVerification.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import useForm from 'react-hook-form';
 import './age.css';
 
+
 const AgeVerification = () => {
+    const { register, handleSubmit, errors } = useForm() // initialise the hook
+    const onSubmit = (data) => { console.log(data) } // callback when validation pass
+
     return (
-        <h1>Hello World</h1>
-    )
+        <form onSubmit={handleSubmit(onSubmit)}>
+            
+            <input type="number" name="age" ref={register({ min: 19 })} /> {/* apply a Refex validation */}
+            {errors.age && 'You must be 19 years old or older to enter this website'} {/* error message */}
+
+            <input type="submit" />
+        </form>
+    )      
 }
 
 export default AgeVerification;
--- buddflix-react/src/App.js
+++ buddflix-react/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          BuddFlix 
+          
           <AgeVerification />
           <Categories />
           <WeedList />
