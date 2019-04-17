7ccc1c0191348759665bec3eed29a59f09289e1c
Ben Lardie <benlardie@gmail.com>
2019-04-17 13:46:04 -0400

    fixed the merge problems

--- buddflix-react/src/App.js
+++ buddflix-react/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import AgeVerification from './AgeVerification/AgeVerification';
 import Navigation from './Navigation/Navigation';
 
