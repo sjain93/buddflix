79a74f4ed503341d20a797431af6602a02557b5c
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-19 12:22:09 -0400

    split the effects into seperate lis

--- buddflix-react/src/Strain.js
+++ buddflix-react/src/Strain.js
@@ -3,11 +3,17 @@ import React from 'react';
 
 const Strain = ({ weed, index }) => {
 
+    const effectList = weed.effects.map((effect) => {
+        return (
+            <li>{effect}</li>
+        )
+    })
+
     return (
         <div className="strains" key={index}>
             <h2 className='strain-name'>{weed.name}</h2>
             <ul>
-                <li>{weed.effects}</li>
+                {effectList}
             </ul>
         </div>
     )
