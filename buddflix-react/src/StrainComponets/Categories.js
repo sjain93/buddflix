a7a6dbea23fbee9eaedfb970f52d9db02897f029
Ben Lardie <benlardie@gmail.com>
2019-04-23 15:44:19 -0400

    scroll now works

--- buddflix-react/src/StrainComponets/Categories.js
+++ buddflix-react/src/StrainComponets/Categories.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useLayoutEffect } from 'react';
+import React, { useEffect, useLayoutEffect, useState, useRef } from 'react';
 import { Spring } from 'react-spring/renderprops'
 import StrainList from './StrainList';
 import axios from 'axios';
@@ -9,6 +9,7 @@ const url = 'http://localhost:8000/api/race/'
 const Categories = () => {
     const [races, setRaces] = useState([]);
     const [selectedRace, setSelectedRace] = useState({});
+    const raceRef = useRef(null);
 
     useEffect(() => {
         axios.get(url).then(response => {
@@ -17,30 +18,27 @@ const Categories = () => {
         });
     }, []);
 
-    const categoriesRef = useRef();
-
-    const scrollToMyRef = () => window.scrollTo(0, categoriesRef)
-    useLayoutEffect(() => {
-        window.scrollTo(0, categoriesRef)
-        scrollToMyRef()
-    },)
+    useEffect(() => {
+        window.scrollTo(0, raceRef.current.offsetTop)
+        // const t = setTimeout(() => window.scrollTo(0, raceRef.current.offsetTop), 280)
+        // return () => clearTimeout(t)
+    }, [races]);
 
     return (
         <Spring
-        from={{opacity: 0, marginTop: -500}}
-        to={{opacity: 1, marginTop: 0}}
+        from={{opacity: 0}}
+        to={{opacity: 1}}
         >
         {props => (
             <div style={props} >
-            <div className="races" ref={categoriesRef} >
-            {races.map(race => (
-                <button ref={categoriesRef} className='btn' onClick={() => setSelectedRace(race)} key={race.id}>
-                    <span>{race.name}</span>
-                </button>
-            ))}
-            </div>
-            <StrainList selectedRace={selectedRace} />
-
+                <div className="races" ref={raceRef} >
+                {races.map(race => (
+                    <button className='btn' onClick={() => setSelectedRace(race)} key={race.id}>
+                        <span>{race.name}</span>
+                    </button>
+                ))}
+                </div>
+                <StrainList selectedRace={selectedRace} />
             </div>
         )}
         </Spring>
