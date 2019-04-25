a449a897dbc110e9f7074adc536d3f51f1945bd2
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-25 08:49:25 -0400

    added a basic about page with simple styles

--- buddflix-react/src/About.js
+++ buddflix-react/src/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './About.scss'
 
 const About = () => {
- return (
-     <h1>Hello About</h1>
- )
+    return (
+        <div className='about-page'>
+            <h1 className='about-header'>About Buddflix</h1>
+            <p className='about-p'>Made by budds, for budds to enjoy budds more.</p>
+        </div>
+    )
 }
 
 export default About;
