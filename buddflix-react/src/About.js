13323e8f494344ee01421467666d04d4917cc1c8
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-27 12:13:52 -0400

    added a picture of the budds to the about page

--- buddflix-react/src/About.js
+++ buddflix-react/src/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import budds from './Images/budds.jpg';
 import './About.scss'
 
 const About = () => {
@@ -6,6 +7,7 @@ const About = () => {
         <div className='about-page'>
             <h1 className='about-header'>About BuddFlix</h1>
             <p className='about-p'>Made by budds, for budds to enjoy budds more.</p>
+            <img src={budds} className='budds' alt='The Budds' />
         </div>
     )
 }
